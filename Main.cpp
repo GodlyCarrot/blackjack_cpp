804d36350540e8ee9884426be2496cab4766b21d
GodlyCarrot <robertlquan3@gmail.com>
2024-09-23 16:14:07 -0500

    9/23/24 4:14pm

--- Main.cpp
+++ Main.cpp
@@ -3,16 +3,31 @@ using namespace std;
 
 class Card {
   public:
+    string suit;
     string name;
     int value;
 
-    Card(string x, int y) {
-      name = x;
-      value = y;
+    Card(string x, string y, int z) {
+      suit = x;
+      name = y;
+      value = z;
+    }
+
+    string getName() {
+      return suit + " " + name;
+    }
+
+    int getValue() {
+      return value;
     }
 };
 
 int main() {
-  cout << "Hello World!";
+  cout << "Hello World! \n";
+
+  Card c1("Test", "Twelve", 12);
+  c1.getName();
+  c1.getValue();
+
   return 0;
 }
